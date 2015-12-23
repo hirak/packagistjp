0f68d93ec00b4d9da5b6b5c83eabc4e4114af528
Hiraku NAKANO <hiraku@tojiru.net>
2015-12-23 17:55:44 +0900

    crontab example

--- 04_application.sh
+++ 04_application.sh
@@ -8,6 +8,8 @@ composer='php70 -dallow_url_fopen=1 -dzend.enable_gc=0 /opt/packagist-crawler/co
 $composer install -o
 $composer config -g github-oauth.github.com 70ae7f3991939e7210c657bc8dca16841255bc1c
 
+cd -
+
 cp {temlate,}/opt/packagist-crawler/config.php
 
-## ここからどうするべきか。。
+crontab < crontab
